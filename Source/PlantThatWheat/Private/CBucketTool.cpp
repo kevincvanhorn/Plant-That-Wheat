a42987b618d35d552fb9cfd4f4b2278be98c3df5
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2019-03-15 06:32:45 -0500

    Attempt - Planet world aligned sand.

---
+++ Source/PlantThatWheat/Private/CBucketTool.cpp
@@ -0,0 +1,4 @@
+
+
+#include "CBucketTool.h"
+
---
+++ Source/PlantThatWheat/Public/CBucketTool.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MultiTool/CMultiTool.h"
+#include "CBucketTool.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANTTHATWHEAT_API ACBucketTool : public ACMultiTool
+{
+	GENERATED_BODY()
+	
+};
